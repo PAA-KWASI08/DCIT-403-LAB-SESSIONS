96957729a061a714d73c29e17ffbbfb2d522ab4d
PAA-KWASI08 <pkbkwapong@st.ug.edu.gh>
2026-01-29 23:30:11 +0000

    submission

--- Lab 1/Agent.py
+++ Lab 1/Agent.py
@@ -1,21 +1,20 @@
+import asyncio
 from spade.agent import Agent
 from spade.behaviour import OneShotBehaviour
-import asyncio
 
-class HelloAgent(Agent):
-    class HelloBehaviour(OneShotBehaviour):
+class MyAgent(Agent):
+    class MyBehaviour(OneShotBehaviour):
         async def run(self):
-            print("Hello! My first SPADE agent is running.")
-            await self.agent.stop()
+            print("Agent is running")
 
     async def setup(self):
-        print("Agent starting...")
-        self.add_behaviour(self.HelloBehaviour())
+        self.add_behaviour(self.MyBehaviour())
 
 async def main():
-    agent = HelloAgent("agent1@localhost", "agentpass")
+    agent = MyAgent("agent1@localhost", "password123")
     await agent.start()
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
+    await agent.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
